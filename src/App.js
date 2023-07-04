ee86d4786f686aeea0642abfcdcb64b7cc21969f
Josias <kologojosias@gmail.com>
2023-07-04 13:00:41 +0100

    json-api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
+    axios
+      .get("https://random-data-api.com/api/users/random_user?size=10")
+      .then((response) => {
+        setUsers(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    // const response = await axios.get(
+    //   "https://random-data-api.com/api/users/random_user?size=10"
+    // );
+    // setUsers(response.data);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={fetchData}>Fetch Random</button>
+      <div className="users">
+        {users.map((user) => (
+          <div className="user">
+            <img src={user.avatar} />
+            <h2>
+              {user.first_name} {user.last_name}
+            </h2>
+            <p>{user.employment.title}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
